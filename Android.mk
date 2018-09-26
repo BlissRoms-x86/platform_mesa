a25c8ed90b04a5e3a4f4313575d9511e71efc91c
Jon West <electrikjesus@gmail.com>
2018-09-26 00:10:13 -0400

    Add LLVM70 support
    Thanks to @fguy

--- Android.mk
+++ Android.mk
@@ -97,9 +97,9 @@ define mesa-build-with-llvm
     $(eval LOCAL_CFLAGS += -DHAVE_LLVM=0x0308 -DMESA_LLVM_VERSION_PATCH=0)) \
   $(if $(filter 8,$(MESA_ANDROID_MAJOR_VERSION)), \
     $(eval LOCAL_CFLAGS += -DHAVE_LLVM=0x0309 -DMESA_LLVM_VERSION_PATCH=0)) \
-  $(if $(filter P,$(MESA_ANDROID_MAJOR_VERSION)), \
-    $(eval LOCAL_CFLAGS += -DHAVE_LLVM=0x0309 -DMESA_LLVM_VERSION_PATCH=0)) \
-  $(eval LOCAL_SHARED_LIBRARIES += libLLVM)
+  $(if $(filter 9,$(MESA_ANDROID_MAJOR_VERSION)), \
+    $(eval LOCAL_CFLAGS += -DHAVE_LLVM=0x0700 -DMESA_LLVM_VERSION_PATCH=0)) \
+  $(eval LOCAL_SHARED_LIBRARIES += libLLVM70)
 endef
 
 # add subdirectories
