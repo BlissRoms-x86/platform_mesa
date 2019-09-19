140845702d10ea58ee21b5a245851389a01fb2e7
Jon West <electrikjesus@gmail.com>
2019-09-19 19:47:48 -0400

    android: build mesa with llvm80 (v3)
    (v1) original pie-x86 commit for mesa 19.0
    
    (v2) changes for mesa 19.2 support after commit
         e538dd6 ("android: fix LLVM version string related building errors")
    
    (v3) Changes for LLVM80 and Android 9 & 10

--- Android.mk
+++ Android.mk
@@ -95,10 +95,10 @@ MESA_ENABLE_LLVM := true
 endif
 
 define mesa-build-with-llvm
-  $(if $(filter $(MESA_ANDROID_MAJOR_VERSION), 4 5 6 7), \
+  $(if $(filter $(MESA_ANDROID_MAJOR_VERSION), 4 5 6 7 8 9 10), \
     $(warning Unsupported LLVM version in Android $(MESA_ANDROID_MAJOR_VERSION)),) \
-  $(eval LOCAL_CFLAGS += -DLLVM_AVAILABLE -DMESA_LLVM_VERSION_STRING=\"7.0\") \
-  $(eval LOCAL_SHARED_LIBRARIES += libLLVM70)
+  $(eval LOCAL_CFLAGS += -DLLVM_AVAILABLE -DMESA_LLVM_VERSION_STRING=\"8.0\") \
+  $(eval LOCAL_SHARED_LIBRARIES += libLLVM80)
 endef
 
 # add subdirectories
